d578dbc6476fe217cd4dea6ca2b298e05f7791ff
Chiranjib Bordoloi <54991549+icodechiranjib@users.noreply.github.com>
2019-10-21 23:36:36 +0530

    Add files via upload
    Code File

---
+++ BEEE_CU_Evaluation_Q3.ino
@@ -0,0 +1,42 @@
+int val=0;
+int c = 0;
+void setup()
+{
+  Serial.begin(9600);
+  for (int i=10;i<=13;i++)
+  {
+  	pinMode(i,OUTPUT);
+  }  
+  pinMode(2,INPUT);
+}
+void loop()
+{
+int i;
+c = analogRead(A0);
+val = digitalRead(2);
+delay(500);
+Serial.print(c);
+Serial.print("  ");
+Serial.println(val);
+  if (val == HIGH) 
+  {       
+    if (c<=340)
+    {
+    	for (i=10;i<=13;i++)
+  		{
+  			digitalWrite(i,HIGH);
+  		} 
+    }
+    else
+    {
+      digitalWrite(13,HIGH);
+    }
+  } 
+  else 
+  {
+    for (i=10;i<=13;i++)
+  		{
+  			digitalWrite(i,LOW);
+  		} 
+  }
+}
